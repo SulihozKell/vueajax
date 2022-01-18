e0cbec66611c914b412780ce593872ebc5e2cf80
Bendeguz <bendeguz@medea.hu>
2022-01-18 17:33:32 +0100

    nem ideális komponens

---
+++ src/components/Statue.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Person</th>
+          <th>Height</th>
+          <th>Price</th>
+          <th>Operation</th>
+        </tr>
+      </thead>
+      <tbody>
+        <StatueItem
+          v-for="statue in statues"
+          v-bind:key="statue.id"
+          :statue="statue"
+        />
+        <button v-if="mod_new" @click="newStatue" :disabled="saving">New</button>
+      </tbody>
+    </table>
+    <!--<button @click="loadData">Load Data</button>-->
+  </div>
+</template>
+
+<script>
+import StatueItem from './StatueItem.vue'
+export default {
+  props: ['statues'],
+  components: {StatueItem},
+  methods: {
+    /*Changed(e) {
+        this.$emit('statue-item-changed', e)
+    }*/
+  }
+}
+</script>
---
+++ src/components/StatueItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <tr>
+        <td>{{ statueItem.id }}</td>
+        <td>{{ statueItem.person }}</td>
+        <td>{{ statueItem.height }}</td>
+        <td>{{ statueItem.price }}</td>
+        <td>
+            <button @click="deleteStatue(statueItem.id)">Delete</button>
+            <button @click="editStatue(statueItem.id)">Edit</button>
+        </td>
+        <br />
+        <td>
+            <input type="hidden" v-model="statueItem.id">
+        </td>
+        <td>
+            <input type="text" v-model="statueItem.person">
+        </td>
+        <td>
+            <input type="number" v-model="statueItem.height">
+        </td>
+        <td>
+            <input type="number" v-model="statueItem.price">
+        </td>
+        <td>
+            
+            <button v-if="!mod_new" @click="saveStatue" :disabled="saving">Save</button>
+            <button v-if="!mod_new" @click="cancelEdit" :disabled="saving">Cancel</button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: ['statue'],
+    data() {
+        return {
+            mod_new: true,
+            saving: false,
+            statueItem: {
+                id: this.statue.id,
+                person: this.statue.person,
+                height: this.statue.height,
+                price: this.statue.price
+            },
+            statues: []
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
